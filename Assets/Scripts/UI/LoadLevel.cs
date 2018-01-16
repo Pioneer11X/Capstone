6aa9fd8baae2a54b3f6ad510910523cf5b9f2d72
Darren <dmf6737@rit.edu>
2018-01-16 12:05:54 -0500

    Started Auido

--- Assets/Scripts/UI/LoadLevel.cs
+++ Assets/Scripts/UI/LoadLevel.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 /// <summary>
 /// Load the main level, once.
 /// </summary>
 public class LoadLevel : MonoBehaviour
 {
+    [SerializeField] private GameObject cam;
+    [SerializeField] private AudioSource audio;
 
-    bool loading;
+    private bool loading;
+    private bool loaded;
+    private bool once;
 
     // Use this for initialization
     void Start()
     {
         loading = false;
+        loaded = false;
+        once = false;
     }
 
     // Update is called once per frame
@@ -20,9 +27,36 @@ public class LoadLevel : MonoBehaviour
     {
         if (!loading)
         {
-            SceneManager.LoadSceneAsync("MainLevel");
-
             loading = true;
+
+            StartCoroutine(LoadYourAsyncScene());
+
+            Debug.Log("called loading");
         }
+
+        if(loaded && !once)
+        {
+            once = true;
+            audio.Stop();
+            cam.SetActive(false);
+            SceneManager.UnloadSceneAsync("LoadingM");
+        }
+    }
+
+    /// <summary>
+    /// Load function from Unity Documentation
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainLevel", LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("MainLevel"));
+        loaded = true;
     }
 }
--- Assets/Scripts/UI/LoadTutorial.cs
+++ Assets/Scripts/UI/LoadTutorial.cs
@@ -1,27 +1,62 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 /// <summary>
 /// Load the tutorial level, once.
 /// </summary>
 public class LoadTutorial : MonoBehaviour
 {
-    bool loading;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private AudioSource audio;
 
-	// Use this for initialization
-	void Start ()
+    private bool loading;
+    private bool loaded;
+    private bool once;
+
+    // Use this for initialization
+    void Start()
     {
         loading = false;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		if (!loading)
-        {
-            SceneManager.LoadSceneAsync("Tutorial");
+        loaded = false;
+        once = false;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (!loading)
+        {
             loading = true;
+
+            StartCoroutine(LoadYourAsyncScene());
+
+            Debug.Log("called loading");
         }
-	}
+
+        if (loaded && !once)
+        {
+            once = true;
+            audio.Stop();
+            cam.SetActive(false);
+            SceneManager.UnloadSceneAsync("LoadingT");
+        }
+    }
+
+    /// <summary>
+    /// Load function from Unity Documentation
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Tutorial", LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Tutorial"));
+        loaded = true;
+    }
 }
