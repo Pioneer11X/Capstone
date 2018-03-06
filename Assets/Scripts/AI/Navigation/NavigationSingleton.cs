10a19cc13b36eb77473956d05a666b3445299f0a
Sravan Karuturi <sravankumarkaruturi@gmail.com>
2018-03-06 15:32:11 -0500

    Working on new Enemy Navigation.

---
+++ Assets/Scripts/AI/Navigation/NavigationSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PathingNode
+{
+    int index;
+    Vector3 nodePosition;
+    PathingNode[] connectedNodes;
+}
+
+public class NavigationSingleton : MonoBehaviour {
+
+    public static NavigationSingleton Instance { get; private set; }
+
+    // 
+    // This is the pointer to the existing nodes. This would be filled upfront on the engine from the exporter.
+    public List<PathingNode> nodes;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+
+}
